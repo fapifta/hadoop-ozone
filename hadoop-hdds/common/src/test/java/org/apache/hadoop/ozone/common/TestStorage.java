06ab1d7d5b8094174d581527e29db541e1fce75d
Istvan Fajth <pifta@cloudera.com>
2020-01-06 03:49:05 +0100

    Add test cases for non-existent storage state after initialization.

--- hadoop-hdds/common/src/test/java/org/apache/hadoop/ozone/common/TestStorage.java
+++ hadoop-hdds/common/src/test/java/org/apache/hadoop/ozone/common/TestStorage.java
@@ -1,37 +1,87 @@
 package org.apache.hadoop.ozone.common;
 
 import static org.junit.Assert.*;
-
+import static org.mockito.Mockito.*;
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos.NodeType;
-import org.apache.hadoop.test.GenericTestUtils;
-import org.junit.Before;
+import org.apache.hadoop.ozone.common.Storage.StorageState;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Properties;
 
 public class TestStorage {
 
-  private File workDir;
-  private Properties props;
-  private Storage storage;
+  @Test
+  public void testStorageInNonExistentStateIfStorageDirectoryIsNotCreated()
+      throws Exception {
+    Storage storage = aStorageImplWith(aNonExistingFile());
 
-  @Before
-  public void setupDummyInstance() throws IOException {
-    workDir = GenericTestUtils.getRandomizedTestDir();
-    workDir.deleteOnExit();
-    storage = new Storage(NodeType.DATANODE, workDir, "test") {
+    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+  }
+
+  @Test
+  public void testStorageInNonExistentStateIfStorageDirectoryIsNotADirectory()
+      throws Exception {
+    Storage storage = aStorageImplWith(aFile());
+
+    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+  }
+
+  @Test
+  public void testStorageInNonExistentStateIfStorageDirectoryIsNotWritable()
+      throws Exception {
+    Storage storage = aStorageImplWith(aNonWritableDirectory());
+
+    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+  }
+
+  @Test
+  public void
+  testStorageInNonExistentStateIfStorageDirectoryAccessThrowsSecurityException()
+      throws Exception {
+    Storage storage = aStorageImplWith(aNonAccessibleFile());
+
+    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+  }
+
+
+
+  private File aNonExistingFile() {
+    return spy(new File("aPath"));
+  }
+
+  private File aFile(){
+    File f = spy(new File("aPath"));
+    doReturn(true).when(f).exists();
+    doReturn(false).when(f).isDirectory();
+    doReturn(true).when(f).isFile();
+    return f;
+  }
+
+  private File aNonWritableDirectory() {
+    File f = spy(new File("aPath"));
+    doReturn(true).when(f).exists();
+    doReturn(true).when(f).isDirectory();
+    doReturn(false).when(f).canWrite();
+    return f;
+  }
+
+  private File aNonAccessibleFile(){
+    File f = spy(new File("aPath"));
+    doThrow(new SecurityException()).when(f).exists();
+    return f;
+  }
+
+  private Storage aStorageImplWith(File workingDir) throws Exception {
+    return aStorageImplWith(workingDir, null);
+  }
+
+  private Storage aStorageImplWith(File workingDir, Properties props)
+      throws Exception {
+    return new Storage(NodeType.DATANODE, workingDir, "test") {
       @Override protected Properties getNodeProperties() {
         return props;
       }
     };
   }
-
-  @Test
-  public void testStorageInNonExistentStateIfStorageDirectoryIsNotCreated(){
-    assertTrue(Storage.StorageState.NON_EXISTENT.equals(storage.getState()));
-  }
-
-
 }

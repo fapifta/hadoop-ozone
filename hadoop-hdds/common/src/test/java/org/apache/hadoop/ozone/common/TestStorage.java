0844946c46ca5210352b1f55cb9fef82077eee08
Istvan Fajth <pifta@cloudera.com>
2020-01-06 16:19:31 +0100

    Add further tests. For initialized and not initialized state. For directory structure.

--- hadoop-hdds/common/src/test/java/org/apache/hadoop/ozone/common/TestStorage.java
+++ hadoop-hdds/common/src/test/java/org/apache/hadoop/ozone/common/TestStorage.java
@@ -2,11 +2,15 @@ package org.apache.hadoop.ozone.common;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos.NodeType;
 import org.apache.hadoop.ozone.common.Storage.StorageState;
+import org.apache.hadoop.test.GenericTestUtils;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Properties;
 
 public class TestStorage {
@@ -16,7 +20,7 @@ public class TestStorage {
       throws Exception {
     Storage storage = aStorageImplWith(aNonExistingFile());
 
-    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+    assertEquals(StorageState.NON_EXISTENT, storage.getState());
   }
 
   @Test
@@ -24,7 +28,7 @@ public class TestStorage {
       throws Exception {
     Storage storage = aStorageImplWith(aFile());
 
-    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+    assertEquals(StorageState.NON_EXISTENT, storage.getState());
   }
 
   @Test
@@ -32,7 +36,7 @@ public class TestStorage {
       throws Exception {
     Storage storage = aStorageImplWith(aNonWritableDirectory());
 
-    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+    assertEquals(StorageState.NON_EXISTENT, storage.getState());
   }
 
   @Test
@@ -41,25 +45,81 @@ public class TestStorage {
       throws Exception {
     Storage storage = aStorageImplWith(aNonAccessibleFile());
 
-    assertTrue(StorageState.NON_EXISTENT.equals(storage.getState()));
+    assertEquals(StorageState.NON_EXISTENT, storage.getState());
+  }
+
+  @Test
+  public void testStorageInInitializedStateIfVersionFileExists()
+      throws Exception {
+    Storage storage =
+        aStorageImplWithRealVersionFile(aRealDirectory(), aValidProperties());
+
+    assertEquals(StorageState.INITIALIZED, storage.getState());
+  }
+
+  @Test
+  public void testStorageInNotInitializedStateIfVersionFileDoesNotExist()
+      throws Exception {
+    Storage storage = aStorageImplWith(aWriteableDirectory());
+
+    assertEquals(StorageState.NOT_INITIALIZED, storage.getState());
+  }
+
+  @Test
+  public void testStorageDirIsTheCompositionOfCtorParameters()
+      throws Exception {
+    File workingDir = aWriteableDirectory();
+    String sdName = aPath();
+
+    Storage storage = new Storage(NodeType.DATANODE, workingDir, sdName) {
+      @Override protected Properties getNodeProperties() {
+        return null;
+      }
+    };
+
+    assertEquals(
+        new File(workingDir, sdName).getAbsoluteFile().toString(),
+        storage.getStorageDir()
+    );
   }
 
 
 
+
+  private String aPath(){
+    return "aPath";
+  }
+
   private File aNonExistingFile() {
-    return spy(new File("aPath"));
+    return spy(new File(aPath()));
   }
 
   private File aFile(){
-    File f = spy(new File("aPath"));
+    File f = spy(new File(aPath()));
     doReturn(true).when(f).exists();
     doReturn(false).when(f).isDirectory();
     doReturn(true).when(f).isFile();
     return f;
   }
 
+  private File aRealDirectory(){
+    File f = GenericTestUtils.getRandomizedTestDir();
+    f.mkdirs();
+    f.deleteOnExit();
+    return f;
+  }
+
+  private File aWriteableDirectory(){
+    File f = spy(new File(aPath()));
+    doReturn(true).when(f).exists();
+    doReturn(true).when(f).isDirectory();
+    doReturn(false).when(f).isFile();
+    doReturn(true).when(f).canWrite();
+    return f;
+  }
+
   private File aNonWritableDirectory() {
-    File f = spy(new File("aPath"));
+    File f = spy(new File(aPath()));
     doReturn(true).when(f).exists();
     doReturn(true).when(f).isDirectory();
     doReturn(false).when(f).canWrite();
@@ -67,9 +127,17 @@ public class TestStorage {
   }
 
   private File aNonAccessibleFile(){
-    File f = spy(new File("aPath"));
+    File f = spy(new File(aPath()));
     doThrow(new SecurityException()).when(f).exists();
     return f;
+  }
+
+  private Properties aValidProperties(){
+    Properties props = new Properties();
+    props.setProperty("nodeType", NodeType.DATANODE.name());
+    props.setProperty("clusterID", "1");
+    props.setProperty("cTime", "0");
+    return props;
   }
 
   private Storage aStorageImplWith(File workingDir) throws Exception {
@@ -77,11 +145,22 @@ public class TestStorage {
   }
 
   private Storage aStorageImplWith(File workingDir, Properties props)
-      throws Exception {
-    return new Storage(NodeType.DATANODE, workingDir, "test") {
-      @Override protected Properties getNodeProperties() {
+      throws IOException {
+    return new Storage(NodeType.DATANODE, workingDir, aPath()) {
+      @Override
+      protected Properties getNodeProperties() {
         return props;
       }
     };
+  }
+
+  private Storage aStorageImplWithRealVersionFile(
+      File workingDir, Properties props) throws Exception {
+    File actualDir = new File (workingDir, aPath() + "/current");
+    actualDir.mkdirs();
+    File versionFile = new File(actualDir, "VERSION");
+    props.store(new FileWriter(versionFile), "no comments needed");
+
+    return aStorageImplWith(workingDir, props);
   }
 }

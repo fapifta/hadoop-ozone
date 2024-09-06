fe6cf1f03ef91c570b6494391f9d366b69fb1aa3
Szabolcs Gál <sgal@cloudera.com>
2024-09-06 15:40:06 +0200

    Use RootCaServer in HASecurityUtils

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/HASecurityUtils.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/HASecurityUtils.java
@@ -25,7 +25,6 @@ import org.apache.hadoop.hdds.scm.server.SCMStorageConfig;
 import org.apache.hadoop.hdds.security.SecurityConfig;
 import org.apache.hadoop.hdds.security.x509.certificate.authority.CertificateServer;
 import org.apache.hadoop.hdds.security.x509.certificate.authority.CertificateStore;
-import org.apache.hadoop.hdds.security.x509.certificate.authority.DefaultCAServer;
 import org.apache.hadoop.hdds.security.x509.certificate.authority.RootCAServer;
 import org.apache.hadoop.hdds.security.x509.certificate.authority.SubCAServer;
 import org.apache.hadoop.hdds.security.x509.certificate.authority.profile.DefaultCAProfile;
@@ -136,10 +135,10 @@ public final class HASecurityUtils {
     String subject = SCM_ROOT_CA_PREFIX +
         InetAddress.getLocalHost().getHostName();
 
-    DefaultCAServer rootCAServer = new DefaultCAServer(subject,
+    RootCAServer rootCAServer = new RootCAServer(subject,
         scmStorageConfig.getClusterID(),
         scmStorageConfig.getScmId(), scmCertStore, pkiProfile, component,
-        rootCertId, "", null);
+        rootCertId, null);
 
     rootCAServer.init(config);
 

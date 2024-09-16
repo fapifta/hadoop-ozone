26bf1447e3ba535c2b1006d7b24f3cd426101fda
Szabolcs Gál <sgal@cloudera.com>
2024-09-16 17:14:04 +0200

    Fix compilation issue caused by ClientestManager being removed from interface

--- hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/x509/certificate/client/CertificateClientTestImpl.java
+++ hadoop-hdds/framework/src/test/java/org/apache/hadoop/hdds/security/x509/certificate/client/CertificateClientTestImpl.java
@@ -48,7 +48,6 @@ import java.util.function.Function;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.apache.hadoop.hdds.conf.OzoneConfiguration;
-import org.apache.hadoop.hdds.scm.client.ClientTrustManager;
 import org.apache.hadoop.hdds.security.exception.SCMSecurityException;
 import org.apache.hadoop.hdds.security.SecurityConfig;
 import org.apache.hadoop.hdds.security.ssl.ReloadingX509KeyManager;
@@ -352,17 +351,6 @@ public class CertificateClientTestImpl implements CertificateClient {
     } catch (IOException | GeneralSecurityException e) {
       throw new CertificateException("Failed to init trustManager", e);
     }
-  }
-
-  @Override
-  public ClientTrustManager createClientTrustManager() throws IOException {
-    CACertificateProvider caCertificateProvider = () -> {
-      List<X509Certificate> caCerts = new ArrayList<>();
-      caCerts.addAll(getAllCaCerts());
-      caCerts.addAll(getAllRootCaCerts());
-      return caCerts;
-    };
-    return new ClientTrustManager(caCertificateProvider, caCertificateProvider);
   }
 
   @Override

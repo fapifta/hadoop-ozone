abacec756dc63abda0b78fed4c09398d6a9c5e53
Szabolcs Gál <sgal@cloudera.com>
2024-10-12 21:48:32 +0200

    Remove unnecessary loadAllCertificates

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/client/DefaultCertificateClient.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/client/DefaultCertificateClient.java
@@ -268,8 +268,6 @@ public abstract class DefaultCertificateClient implements CertificateClient {
     if (sslIdentityStorage == null) {
       getLogger().info("SSLIdentityStorage not yet initialized, reiniting with certId: {}", certSerialId);
       sslIdentityStorage = new SSLIdentityStorage(securityConfig, component, certSerialId);
-      //This has a side effect of filling up the certificate maps that's needed for now
-      loadAllCertificates();
     }
     if (sslIdentityStorage.getCertPaths().isEmpty()) {
       getLogger().info("No certificates found for certificate client with certificate id: {}", certSerialId);
